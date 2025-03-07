f09164bc6d34352ea6e8f469c8742e5e068542da
SiddTripathi <siddhart.tripathi@gmail.com>
2025-03-07 17:35:07 +1300

    Python Variables introduction

---
+++ Python_basics/variables_python.py
@@ -0,0 +1,12 @@
+#there is nothing like data type declaration. It adapts the data type according to the value assigned to it.
+#Python is a dynamically typed language
+#python first creates the value on right side of variable then assigns it to the variable on left side.
+
+
+name = "John"
+age = 23
+name2 = "Ravi"
+age2 = 24
+
+print(name+name2)
+print(age+age2)
