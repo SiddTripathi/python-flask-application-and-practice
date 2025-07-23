a13ebff1c8b06e635f769cbcc643d2bf504f619e
SiddTripathi <siddhart.tripathi@gmail.com>
2025-07-23 12:23:00 +1200

    test 1

--- Flask_App_Store/models/__init__.py
+++ Flask_App_Store/models/__init__.py
@@ -1,9 +1,9 @@
 
-from stores import StoreModel
-from items import ItemModel
-from tag import TagModel
-from user import UserModel
-from item_tags import ItemTags
+from .stores import StoreModel
+from .items import ItemModel
+from .tag import TagModel
+from .user import UserModel
+from .item_tags import ItemTags
 
 
 #these imports above help import models anywhere else easily simply by saying from models import storemodel instead of writing the entire models.stores etc.
