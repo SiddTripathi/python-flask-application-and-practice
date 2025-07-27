02a746dd29a83eb89e61a98272faf2175455c4d2
SiddTripathi <siddhart.tripathi@gmail.com>
2025-07-28 11:53:36 +1200

    Added logic for sending email on user registration

--- Flask_App_Store/app.py
+++ Flask_App_Store/app.py
@@ -5,6 +5,7 @@ from flask_jwt_extended import JWTManager
 from flask_migrate import Migrate
 from blocklist import BLOCKLIST
 from flask_smorest import Api
+
 from db import db
 from dotenv import load_dotenv
 #from .models import StoreModel, ItemModel
