4230a4c92705702742a86443c1504f64b541b60e
SiddTripathi <siddhart.tripathi@gmail.com>
2025-07-28 15:53:16 +1200

    Changes to DB

--- Flask_App_Store/app.py
+++ Flask_App_Store/app.py
@@ -20,7 +20,7 @@ from resources.users import blp as UserBlueprint
 def create_app(db_url=None):
     instance_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "instance") #--> this ensures folder is created inside current working dir
     app = Flask(__name__, instance_path=instance_path)
-    load_dotenv()
+    #load_dotenv()
 
 
 
