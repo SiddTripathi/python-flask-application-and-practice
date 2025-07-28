12e981da87cd13cf3b1d0484c5451dd989474eeb
SiddTripathi <siddhart.tripathi@gmail.com>
2025-07-29 09:02:10 +1200

    Email not unique

--- Flask_App_Store/models/user.py
+++ Flask_App_Store/models/user.py
@@ -5,6 +5,6 @@ class UserModel(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
-    email = db.Column(db.String, unique=True, nullable=False)
+    email = db.Column(db.String, nullable=False)
     password = db.Column(db.String(), nullable=False)
     is_admin = db.Column(db.Boolean, default=False, nullable=False)
